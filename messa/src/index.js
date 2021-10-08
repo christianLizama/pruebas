b9217e24c9d35628c372104ef298fdda307577c9
Diego Aguilera <diegoaguilera4@icloud.com>
2021-10-07 23:02:34 -0300

    porsiacaso

---
+++ messa/src/index.js
@@ -0,0 +1,15 @@
+const express=require('express');
+const morgan=require('morgan');
+const cors=require('cors');
+const app=express();
+app.use(morgan('dev'));
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.set('port',process.env.PORT || 3000);
+
+app.listen(app.get('port'),()=>{
+    console.log('sv port '+ app.get('port'));
+});
